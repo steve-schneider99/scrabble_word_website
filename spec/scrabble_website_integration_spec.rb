ac35375daacc04a96c6c0d8e69bc7fecb1d1095b
Franz Knupfer & Steve Schneider <student@epicodus.com>
2015-04-22 13:36:27 -0700

    finish Scrabble score program and run specs

--- spec/scrabble_website_integration_spec.rb
+++ spec/scrabble_website_integration_spec.rb
@@ -2,11 +2,11 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 
-describe('the Scrabble score path', {:type=> :feature} do
+describe('the Scrabble score path', {:type=> :feature}) do
   it('processes the user entry and returns the the word and the score of the word') do
     visit('/')
-    fill_in('Scrabble_word', :with=> 'cat')
-    click_button('Send')
-    expect(page).to have_content(WORD: "cat", SCORE: 5)
+    fill_in('word', :with=> 'cat')
+    click_button('Score the word!')
+    expect(page).to have_content(5)
   end
 end
